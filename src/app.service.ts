9678b8792a1b8adcd00503584af6567bfa0ff0c6
badongit <nguyenbadong01@gmail.com>
2024-03-16 14:12:24 +0700

    add workflows action cicd

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! I am working with Docker';
+    return 'Hello World! I am working with Docker! Now i am testing cicd for this app.';
   }
 }
